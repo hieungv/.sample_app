6b26ffe8fabb98c63987064607b20f195e3c592b
Nguyen Van Hieu <hieungv.hn@gmail.com>
2019-01-15 13:02:39 +0700

    Chapter 9: Advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  attr_reader :remember_token
   validates :name, presence: true, length: {maximum: Settings.max_length_user}
   validates :email, presence: true,length: {maximum: Settings.max_length_email},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
@@ -8,14 +9,32 @@ class User < ApplicationRecord
   before_save :downcase_email
   has_secure_password
 
-  private
-
   def downcase_email
     email.downcase!
   end
 
-  def User.digest string
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update remember_digest: nil
   end
 end

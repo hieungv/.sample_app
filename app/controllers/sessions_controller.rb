561bc51dae99391ba71ca2ece97d7706558ea98a
Nguyen Van Hieu <hieungv.hn@gmail.com>
2019-01-15 13:02:39 +0700

    Chapter 11: Account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,31 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
 
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == Settings.one ? remember(user) : forget(user)
-      redirect_back_or user
+    if user&.authenticate(params[:session][:password])
+      if user.activated?
+        acc_activated user
+      else
+        acc_not_activated
+      end
     else
-      flash.now[:danger] = t("sessions_controller.er")
+      flash.now[:danger] = t "sessions_controller.er"
       render :new
     end
   end
 
   def destroy
     log_out if logged_in?
+    redirect_to root_url
+  end
+
+  def acc_activated user
+    log_in user
+    params[:session][:remember_me] == Settings.one ? remember(user) : forget(user)
+    redirect_back_or user
+  end
+
+  def acc_not_activated
+    flash[:warning] = t "message"
     redirect_to root_url
   end
 end
